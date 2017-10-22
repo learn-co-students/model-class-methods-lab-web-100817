f0889478e24a3dad8cca8bcdf6e13ce04e02fd26
Dakota Lillie <dakota.lillie@icloud.com>
2017-10-21 21:01:58 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,22 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: [:classifications]).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    self.joins(boats: [:classifications]).where("classifications.name = 'Sailboat'").uniq
+  end
+
+  def self.talented_seamen
+    talented_seamen_array = self.joins(boats: [:classifications]).where("classifications.name = 'Sailboat'") & self.joins(boats: [:classifications]).where("classifications.name = 'Motorboat'")
+    self.where(id: talented_seamen_array.collect(&:id))
+  end
+
+  def self.non_sailors
+    sailors = self.joins(boats: [:classifications]).where("classifications.name = 'Sailboat'").uniq
+    non_sailors_array = self.all - sailors
+    self.where(id: non_sailors_array.collect(&:id))
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    max_length = Boat.maximum("length")
+    max_boat = Boat.find_by(length: max_length)
+    max_boat.classifications
+  end
 end
