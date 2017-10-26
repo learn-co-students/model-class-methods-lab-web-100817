65326e2af1794736f35717208fdbcb6e1ea7aa56
github username <github email address>
2017-10-26 17:04:33 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,37 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    boats = Boat.joins(:classifications).where('classifications.name = "Catamaran"').map {|boat| boat.captain_id}
+    Captain.where(id: boats)
+  end
+
+  def self.sailors
+      boats = Boat.joins(:classifications).where('classifications.name = "Sailboat"').map {|boat| boat.captain_id}
+      Captain.where(id: boats)
+    end
+
+  def self.talented_seamen
+    boats = Boat.joins(:classifications).where('classifications.name = "Motorboat"').map {|boat| boat.captain_id}
+    boats2 = Boat.joins(:classifications).where('classifications.name = "Sailboat"').map {|boat| boat.captain_id}
+    array = []
+    boats.each do |boat|
+      if boats2.include?(boat)
+        array << boat
+      end
+    end
+    Captain.where(id: array)
+  end
+
+  def self.non_sailors
+    # captain ids: [8323, 8324, 8325, 8326, 8327, 8328, nil]
+      #  expected: ["William Kyd", "Arel English", "Henry Hudson"]
+      #       got: ["Captain Cook", "Captain Kidd", "William Kyd", "Arel English", "Henry Hudson", "Samuel Axe"]
+    sailboat_captains = Boat.joins(:classifications).where('classifications.name = "Sailboat"').map {|boat| boat.captain_id}
+    Captain.where.not(id: sailboat_captains)
+  end
+
+  def self.my_all
+      Boat.joins(:classifications).map {|classif| classif.name}
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    order("id")
+  end
+
+  def self.longest
+    name = Classification.joins(:boats).where("boats.length = ?", Boat.maximum("length"))
+  end
+
 end
